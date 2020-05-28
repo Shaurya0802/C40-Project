9638a21a9ebe1de280b290af65e75d8951558167
Shaurya0802 <62699422+Shaurya0802@users.noreply.github.com>
2020-05-28 19:54:17 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,94 @@
+class Game{
+    constructor(){}
+ 
+    getGameState(){
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function(data){
+            gameState = data.val();
+        })
+    }
+
+    update(state){
+        database.ref('/').update({
+            gameState : state
+        });
+    }
+
+    async start(){
+        if(gameState === 0){
+            player = new Player();
+            var playerCountRef = await database.ref('playerCount').once("value");
+            if(playerCountRef.exists()){
+                playerCount = playerCountRef.val();
+                player.getCount();
+            }
+            form = new Form;
+            form.display();
+        }
+
+        player1 = createSprite(100,200);
+        player1.addImage("img1",player1Img);
+        player1.scale = 0.3;
+        player2 = createSprite(300,200);
+        player2.addImage("img2",player2Img);
+        player2.scale = 0.2;
+        player3 = createSprite(500,200);
+        player3.addImage("img3",player3Img);
+        player3.scale = 0.3;
+        player4 = createSprite(700,200);
+        player4.addImage("img4",player4Img);
+        player4.scale = 0.3;
+        players = [player1, player2, player3, player4];
+    }
+
+    play(){
+        form.hide();
+
+        Player.getPlayerInfo();
+        Player.getPlayersAtEnd();
+
+        if(allPlayers !== undefined){
+            background('#c68767');
+
+            image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
+
+            var index = 0;
+            var x = 175;
+            var y;
+
+            for(var plr in allPlayers){
+                index = index + 1;
+
+                x = x + 200;
+                y = displayHeight - allPlayers[plr].distance;
+
+                players[index - 1].x = x;
+                players[index - 1].y = y;
+
+                if(index === player.index){
+                    players[index-1].shapeColor = "red";
+                    camera.position.x = displayWidth/2;
+                    camera.position.y = players[index-1].y;
+                }
+            }
+        }
+
+        if(keyIsDown(UP_ARROW) && player.index !== null){
+            player.distance += 10;
+            player.update();
+        }
+
+        if(player.distance > 3860){
+            gameState = 2;
+            player.rank += 1;
+            Player.updatePlayersAtEnd(player.rank);
+        }
+
+        drawSprites();
+    }
+
+    end(){
+        game.update(2);
+        console.log("Game Ended");
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,45 @@
+var canvas;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form,player,game;
+
+var allPlayers;
+
+var players, player1, player2, player3, player4;
+
+var player1Img,player2Img,player3Img,player4Img;
+
+var track,ground;
+
+function preload(){
+    track = loadImage("images/track.jpg");
+    ground = loadImage("images/ground.png");
+    player1Img = loadImage("images/Player2.gif");
+    player2Img = loadImage("images/Player1.gif");
+    player3Img = loadImage("images/Player3.png");
+    player4Img = loadImage("images/Player1.png");
+}
+
+function setup(){
+    canvas = createCanvas(displayWidth - 5, displayHeight -5);
+    database = firebase.database();
+
+    game = new Game();
+    game.getGameState();
+    game.start();
+}
+
+function draw(){
+    if(playerCount === 4){
+        game.update(1);
+    }
+
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
